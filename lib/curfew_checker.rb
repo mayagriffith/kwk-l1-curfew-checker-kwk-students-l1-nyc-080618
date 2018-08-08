69219e6e5382db232f31e19a0bb8dc0b95d7e5b2
mayagriffith <mayaegriffith@gmail.com>
2018-08-08 13:48:55 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,5 +1,9 @@
+time = 13
 def simple_curfew_checker(time)
-  # code goes here
+  if time < 23 
+      puts "It is not curfew yet"
+  else 
+    puts "Go back to bed! Curfew!"
 end
 
 def curfew_checker(time)
